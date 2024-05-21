437ba2d1fe0df44704d4ad6b07218b5b3f2ae9eb
Andrii Zemliankin <andrii.zemliankin@pptexcellence.com>
2024-05-21 20:56:15 +0200

    chore: Update API URLs in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // api_url: 'http://192.168.129.29:3000/api',
-  api_url: 'https://553f-2a02-a03f-845c-8b01-c009-8347-c7f3-f6a6.ngrok-free.app/api'
+  api_url: 'http://192.168.129.29:3000/api',
+  // api_url: 'https://f75b-2a02-a03f-845c-8b01-c009-8347-c7f3-f6a6.ngrok-free.app/api'
 };
